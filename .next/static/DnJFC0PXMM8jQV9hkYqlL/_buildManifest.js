1dcff3a6dc00db687ff6f820f0d2012c874e2585
d3inx <mr.reza.rm8224@gmail.com>
2022-05-23 21:51:59 +0430

    main

--- .next/static/DnJFC0PXMM8jQV9hkYqlL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-0079ab64284695c7.js","static/chunks/pages/index-a6244d57acac54cf.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import LatestProjects from "../components/LatestProjects";
 import PersonalCard from "../components/PersonalCard";
 import SkillExperience from "../components/SkillExperience";
@@ -5,6 +6,38 @@ import SkillExperience from "../components/SkillExperience";
 export default function Main() {
   return (
     <>
+      <Head>
+        <title>
+          Reza Manouchehri&apos;s personal website | وب سایت شخصی رضا منوچهری
+        </title>
+        <meta name="author" content="reza manouchehri"></meta>
+        <meta name="description" content="وب سایت شخصی رضا منوچهری"></meta>
+        <meta
+          name="keywords"
+          content="reaza manouchehri, رضا منوچهری, وبسایت رضا منوچهری"
+        ></meta>
+        <meta property="og:title" content="وبسایت شخصی رضا منوچهری" />
+        <meta property="og:site_name" content="سایت رضا منوچهری" />
+        <meta name="theme-color" content="#fbbf24"></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon_io/site.webmanifest"></link>
+      </Head>
       <div className="w-full flex items-center justify-center">
         <PersonalCard />
       </div>
